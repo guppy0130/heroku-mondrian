dd4bf57162f5c38ca723008f9e7e54e8cbaf7760
guppy0130 <guppy0130@yahoo.com>
2018-07-03 14:56:06 -0700

    adding strokes

--- index.js
+++ index.js
@@ -64,12 +64,12 @@ app
         console.log(args);
         try {
             containerTree = splitContainer(new Container(0, 0, args.width, args.height), args.levels, args);
-            graphic = gm(args.width, args.height, settings.colors.white).quality(100);
+            graphic = gm(args.width, args.height, settings.colors.white).quality(100).stroke(settings.colors.black, 3);
 
             console.log('painting...');
             containerTree.paint(graphic);
 
-            console.log('sending image');
+            console.log(graphic);
             graphic.stream('jpg', (err, stdout) => {
                 res.set('Content-Type', 'image/jpg');
                 stdout.pipe(res);
